78ce6f2a0f29b832a705bb93b4070e43ec498d73
phobicadz <chamberlain_adam@hotmail.com>
2015-08-25 21:40:05 +0100

    .

--- UniversalApp1/js/default.js
+++ UniversalApp1/js/default.js
@@ -24,5 +24,33 @@
 		// If you need to complete an asynchronous operation before your application is suspended, call args.setPromise().
 	};
 
+    var itemArray = [
+        { title: "Marvelous Mint", text: "Gelato", picture: "/images/fruits/60Mint.png" },
+        { title: "Succulent Strawberry", text: "Sorbet", picture: "/images/fruits/60Strawberry.png" },
+        { title: "Banana Blast", text: "Low-fat frozen yogurt", picture: "/images/fruits/60Banana.png" },
+        { title: "Lavish Lemon Ice", text: "Sorbet", picture: "/images/fruits/60Lemon.png" },
+        { title: "Creamy Orange", text: "Sorbet", picture: "/images/fruits/60Orange.png" },
+        { title: "Very Vanilla", text: "Ice Cream", picture: "/images/fruits/60Vanilla.png" },
+        { title: "Banana Blast", text: "Low-fat frozen yogurt", picture: "/images/fruits/60Banana.png" },
+        { title: "Lavish Lemon Ice", text: "Sorbet", picture: "/images/fruits/60Lemon.png" }
+];
+
+var items = [];
+
+// Generate 160 items
+for (var i = 0; i < 20; i++) {
+    itemArray.forEach(function (item) {
+        items.push(item);
+    });
+}
+
+
+WinJS.Namespace.define("Sample.ListView", {
+    data: new WinJS.Binding.List(items)
+});
+WinJS.UI.processAll();
+
+
+
 	app.start();
 })();
