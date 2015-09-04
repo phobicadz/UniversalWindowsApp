551c62573daedd4f2e99bc7243860e01ddcb1cdf
norofolknchance <chamberlain_adam@hotmail.com>
2015-09-04 16:55:47 +0100

    .

--- UniversalApp1/js/options.js
+++ UniversalApp1/js/options.js
@@ -32,14 +32,14 @@
         }
     };
 
-    var header = WinJS.UI.Pages.define("/sample-utils/header.html", {
+    var header = WinJS.UI.Pages.define("/html/header.html", {
         processed: function (element, options) {
             return WinJS.Binding.processAll(element);
         }
     });
 
     // Control that populates and runs the scenario selector
-    var ScenarioSelect = WinJS.UI.Pages.define("/sample-utils/scenario-select.html", {
+    var ScenarioSelect = WinJS.UI.Pages.define("/html/options.html", {
         ready: function (element, options) {
             var that = this;
 
@@ -70,7 +70,7 @@
             this._selectedIndex = 0;
 
             var lastUrl = WinJS.Application.sessionState.lastUrl;
-            SdkSample.scenarios.forEach(function (s, index) {
+            MyFirstWUA.scenarios.forEach(function (s, index) {
                 s.scenarioNumber = index + 1;
                 if (s.url === lastUrl && index !== that._selectedIndex) {
                     that._selectedIndex = index;
@@ -100,4 +100,35 @@
             scenarioControl.elementFromIndex(scenarioIndex).click();
         }
     };
+
+    var scenarios = [
+        { url: "/html/enterPage.html", title: "Show page" },
+        { url: "/html/transitionPages.html", title: "Transition between pages" },
+        { url: "/html/enterContent.html", title: "Show content" },
+        { url: "/html/transitionContents.html", title: "Transition between content" },
+        { url: "/html/expandAndCollapse.html", title: "Expand and collapse" },
+        { url: "/html/pointerFeedback.html", title: "Tap and click feedback" },
+        { url: "/html/addAndDeleteFromList.html", title: "Add and remove from list" },
+        { url: "/html/filterSearchList.html", title: "Filter search list" },
+        { url: "/html/fadeInAndOut.html", title: "Fade in and out" },
+        { url: "/html/crossfade.html", title: "Crossfade" },
+        { url: "/html/reposition.html", title: "Reposition" },
+        { url: "/html/dragAndDrop.html", title: "Drag and drop" },
+        { url: "/html/dragBetween.html", title: "Drag between to reorder" },
+        { url: "/html/showPopupUI.html", title: "Show pop-up UI" },
+        { url: "/html/showEdgeUI.html", title: "Show edge UI" },
+        { url: "/html/showPanel.html", title: "Show panel" },
+        { url: "/html/swipeReveal.html", title: "Reveal ability to swipe" },
+        { url: "/html/swipeSelection.html", title: "Swipe select and deselect" },
+        { url: "/html/updateBadge.html", title: "Update a badge" },
+        { url: "/html/updateTile.html", title: "Update a tile" },
+        { url: "/html/customAnimation.html", title: "Run a custom animation" },
+        { url: "/html/disableAnimations.html", title: "Disable animations" }
+    ];
+
+    WinJS.Namespace.define("MyFirstWUA", {
+        scenarios: new WinJS.Binding.List(scenarios)
+    });
+
+
 })();
